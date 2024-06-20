5aa76a91ad544e0b9d2e71aea1c272ddf07e94a6
CIR1996 <2310781015@fh-burgenland.at>
2024-06-21 01:31:46 +0200

    implemented logs

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/health')
+def health_check():
+    return 'OK', 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
